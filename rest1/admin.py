356d6a4870cd36355925d18d17f178c2e1b6e2ec
jardelgomes95 <80551563+jardelgomes95@users.noreply.github.com>
2025-12-02 00:46:24 -0300

    Update admin.py

--- rest1/admin.py
+++ rest1/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import Participante
 
-from django.views.decorators.csrf import csrf_exempt
-
 
 # Configura como a tabela aparece para o Admin
-@csrf_exempt
+
 @admin.register(Participante)
 class ParticipanteAdmin(admin.ModelAdmin):
     list_display = ('nome', 'data_envio')  # Colunas que aparecem na lista
